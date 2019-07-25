ecb599831dd0edd7e8bd0e74ae361524571c8222
joonmo.park <jmpark6846@gmail.com>
2019-07-25 21:29:32 +0900

    기본 레이아웃 추가

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="navbar">
+      <div className="title">notes</div>
+      <ul>
+        <li>Sign Up</li>
+        <li>Sign In</li>
+      </ul>
+    </header>
+  );
+}
---
+++ src/Components/NoteList.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+export default class NoteList extends Component {
+  state = {
+    notes: {
+      1: {
+        id: 1,
+        content:
+          "Two of the creators of Tailwind Adam Watham and Steve Schoger, the masterminds behind Refactoring"
+      },
+      2: {
+        id: 2,
+        content:
+          "Today we can't imagine coding without our favorite CSS framework"
+      },
+      3: {
+        id: 3,
+        content:
+          "Semantic is a development framework that helps create beautiful, responsive"
+      },
+      4: {
+        id: 4,
+        content:
+          "Two of the creators of Tailwind Adam Watham and Steve Schoger, the masterminds behind Refactoring"
+      },
+      5: {
+        id: 5,
+        content:
+          "Today we can't imagine coding without our favorite CSS framework"
+      }
+    }
+  };
+  render() {
+    const { notes } = this.state;
+    return (
+      <section className="content container">
+        <div className="single-column">
+          {Object.values(notes).map(note => (
+            <div className="note">{note.content}</div>
+          ))}
+        </div>
+      </section>
+    );
+  }
+}
