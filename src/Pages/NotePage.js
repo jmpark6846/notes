4b04f954407899c9feed4982d22c17c77e947463
joonmo.park <jmpark6846@gmail.com>
2019-07-26 15:20:37 +0900

    사이드바에 컴포넌트 추가

--- src/Pages/NotePage.js
+++ src/Pages/NotePage.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Pane, majorScale, minorScale, Heading } from "evergreen-ui";
+import { Pane, majorScale, minorScale, Heading, Menu, Text, Button } from "evergreen-ui";
 
 export default class NotePage extends Component {
   render() {
@@ -12,18 +12,54 @@ export default class NotePage extends Component {
           display="flex"
           flexDirection="column"
         >
-          <Pane padding={minorScale(4)} background="tint1">
-            control panel
+          <Pane paddingX={minorScale(4)} background="tint1">
+            <Menu>
+              <Menu.Group>
+                <Menu.Item icon="search">검색하기</Menu.Item>
+              </Menu.Group>
+            </Menu>
           </Pane>
-          <Pane padding={minorScale(4)} background="blueTint" flex={1}>
-            Page sidebar
+          <Pane paddingX={minorScale(4)} background="blueTint" flex={1} overflowX="hidden" overflowY="auto">
+            <Menu>
+              <Menu.Group>
+              <Menu.Item><Text>Getting Started</Text></Menu.Item>
+                <Menu.Item><Text>Getting Started</Text></Menu.Item>
+                <Menu.Item><Text>Getting Started</Text></Menu.Item>
+                <Menu.Item><Text>Getting Started</Text></Menu.Item>
+                <Menu.Item><Text>Getting Started</Text></Menu.Item>
+                <Menu.Item><Text>Getting Started</Text></Menu.Item>
+                <Menu.Item><Text>Getting Started</Text></Menu.Item>
+                <Menu.Item><Text>Getting Started</Text></Menu.Item>
+                <Menu.Item><Text>Getting Started</Text></Menu.Item>
+                <Menu.Item><Text>Getting Started</Text></Menu.Item>
+                <Menu.Item><Text>Getting Started</Text></Menu.Item>
+                <Menu.Item><Text>Getting Started</Text></Menu.Item>
+                <Menu.Item><Text>Getting Started</Text></Menu.Item>
+                <Menu.Item><Text>Getting Started</Text></Menu.Item>
+                <Menu.Item><Text>Getting Started</Text></Menu.Item>
+                <Menu.Item><Text>Getting Started</Text></Menu.Item>
+                <Menu.Item><Text>Getting Started</Text></Menu.Item>
+                <Menu.Item><Text>Getting Started</Text></Menu.Item>
+                <Menu.Item><Text>Getting Started</Text></Menu.Item>
+                <Menu.Item><Text>Getting Started</Text></Menu.Item>
+                <Menu.Item><Text>Getting Started</Text></Menu.Item>
+                <Menu.Item><Text>Getting Started</Text></Menu.Item>
+                <Menu.Item><Text>Getting Started</Text></Menu.Item>
+                <Menu.Item><Text>Getting Started</Text></Menu.Item>
+                <Menu.Item><Text>Getting Started</Text></Menu.Item>
+              </Menu.Group>
+            </Menu>
           </Pane>
           <Pane
-            padding={minorScale(4)}
-            height={majorScale(7)}
+            paddingX={minorScale(4)}
             background="tint1"
           >
-            button
+            <Menu>
+              <Menu.Group>
+                <Menu.Item icon="edit">새 노트 작성하기</Menu.Item>
+              </Menu.Group>
+            </Menu>
+            {/* <Button margin="auto" width="100%" display="block" height={40} appearance="minimal" iconBefore="edit" background="tint2">노트 추가하기</Button> */}
           </Pane>
         </Pane>
         <Pane flex={1} display="flex" flexDirection="column">
