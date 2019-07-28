edb9dd5b2d02d61fc78b92e6db9f2992a859036c
joonmo.park <jmpark6846@gmail.com>
2019-07-28 12:38:12 +0900

    노트 제목 변경 시 저장

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
 import NotePage from "./Pages/NotePage";
-import './App.css'
+import "./App.css";
+
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyD70mOFNDK4VjoQOw9JBItSw0_aME3EeWw",
+  authDomain: "notes-d5c0d.firebaseapp.com",
+  databaseURL: "https://notes-d5c0d.firebaseio.com",
+  projectId: "notes-d5c0d",
+  storageBucket: "notes-d5c0d.appspot.com",
+  messagingSenderId: "1001740796289",
+  appId: "1:1001740796289:web:4c92c4b3621037cc"
+});
+
+export const db = firebase.firestore();
+
 function App() {
-  return (
-    <NotePage />
-  )
+  return <NotePage />;
 }
 
 export default App;
--- src/Pages/NotePage.js
+++ src/Pages/NotePage.js
@@ -1,115 +1,102 @@
 import React, { Component } from "react";
-import {
-  Pane,
-  majorScale,
-  minorScale,
-  Heading,
-  Menu,
-  Text,
-  Button
-} from "evergreen-ui";
+import { Pane, minorScale, Heading, Menu, Text } from "evergreen-ui";
+import { Editor } from "slate-react";
+import { Value } from "slate";
+import uuid from 'uuid/v4'
+import { db } from "../App";
 
 export default class NotePage extends Component {
+  state = {
+    selected: "",
+    notes: {},
+    noteTitle: {},
+    isLoading: true
+  };
+
+  async componentDidMount() {
+    const initialValue = {
+      document: {
+        nodes: [
+          {
+            object: "block",
+            type: "paragraph",
+            nodes: [
+              {
+                object: "text",
+                text: "A line of text in a paragraph."
+              }
+            ]
+          }
+        ]
+      }
+    };
+
+    let res = await db
+      .collection("notes")
+      .orderBy("createdAt")
+      .limit(1)
+      .get();
+    let latestNote = res.docs[0].data();
+    console.log(Value.fromJSON(JSON.parse(latestNote.title)))
+    this.setState({
+      selected: latestNote.id,
+      noteTitle: Value.fromJSON(JSON.parse(latestNote.title) || initialValue),
+      isLoading: false
+    });
+  }
+
+  _handleNoteSelect = noteId => {
+    this.setState({
+      selected: noteId
+    });
+  };
+
+  _handleEditorChange = async ({ value }) => {
+    if (value.document != this.state.noteTitle.document) {
+      let res = await db.collection("notes").doc(this.state.selected).update({
+        title: JSON.stringify(value.toJSON())
+      })
+
+      console.log(res)
+    }
+    this.setState({ noteTitle: value });
+  };
+
   render() {
+    const { notes, noteTitle, isLoading, selected } = this.state;
     return (
       <Pane display="flex" height="100%">
         <Pane width={240} height="100%" background="tint1" className="sidebar">
-          {/* <Pane paddingX={minorScale(4)} background="tint1"> */}
-          <Menu> {/* display: flex */}
+          <Menu>
+            {/* display: flex */}
             <Pane>
               <Menu.Group>
                 <Menu.Item icon="search">검색하기</Menu.Item>
               </Menu.Group>
             </Pane>
-            {/* </Menu> */}
-            {/* </Pane>
-          <Pane paddingX={minorScale(4)} background="blueTint" flex={1} overflowX="hidden" overflowY="auto"> */}
-            {/* <Menu> */}
-            <Pane className="note-list" flex={1} overflowX="hidden" overflowY="auto">
+            <Pane
+              className="note-list"
+              flex={1}
+              overflowX="hidden"
+              overflowY="auto"
+            >
               <Menu.Group>
-                <Menu.Item>
-                  <Text>Getting Started</Text>
-                </Menu.Item>
-                <Menu.Item>
-                  <Text>Getting Started</Text>
-                </Menu.Item>
-                <Menu.Item>
-                  <Text>Getting Started</Text>
-                </Menu.Item>
-                <Menu.Item>
-                  <Text>Getting Started</Text>
-                </Menu.Item>
-                <Menu.Item>
-                  <Text>Getting Started</Text>
-                </Menu.Item>
-                <Menu.Item>
-                  <Text>Getting Started</Text>
-                </Menu.Item>
-                <Menu.Item>
-                  <Text>Getting Started</Text>
-                </Menu.Item>
-                <Menu.Item>
-                  <Text>Getting Started</Text>
-                </Menu.Item>
-                <Menu.Item>
-                  <Text>Getting Started</Text>
-                </Menu.Item>
-                <Menu.Item>
-                  <Text>Getting Started</Text>
-                </Menu.Item>
-                <Menu.Item>
-                  <Text>Getting Started</Text>
-                </Menu.Item>
-                <Menu.Item>
-                  <Text>Getting Started</Text>
-                </Menu.Item>
-                <Menu.Item>
-                  <Text>Getting Started</Text>
-                </Menu.Item>
-                <Menu.Item>
-                  <Text>Getting Started</Text>
-                </Menu.Item>
-                <Menu.Item>
-                  <Text>Getting Started</Text>
-                </Menu.Item>
-                <Menu.Item>
-                  <Text>Getting Started</Text>
-                </Menu.Item>
-                <Menu.Item>
-                  <Text>Getting Started</Text>
-                </Menu.Item>
-                <Menu.Item>
-                  <Text>Getting Started</Text>
-                </Menu.Item>
-                <Menu.Item>
-                  <Text>Getting Started</Text>
-                </Menu.Item>
-                <Menu.Item>
-                  <Text>Getting Started</Text>
-                </Menu.Item>
-                <Menu.Item>
-                  <Text>Getting Started</Text>
-                </Menu.Item>
-                <Menu.Item>
-                  <Text>Getting Started</Text>
-                </Menu.Item>
+                {Object.values(notes).map(note => (
+                  <Menu.Item
+                    key={note.id}
+                    onSelect={() => this._handleNoteSelect(note.id)}
+                  >
+                    {note.title}
+                  </Menu.Item>
+                ))}
               </Menu.Group>
             </Pane>
-            {/* </Menu> */}
-            {/* </Pane>
-          <Pane padding={minorScale(4)} background="tint1"> */}
-            {/* <Menu> */}
             <Pane>
               <Menu.Group>
-                <Menu.Item icon="edit">
-                  새 노트 작성하기
-                </Menu.Item>
+                <Menu.Item icon="edit">새 노트 작성하기</Menu.Item>
               </Menu.Group>
             </Pane>
           </Menu>
-
-          {/* <Button margin="auto" width="100%" display="block" height={40} appearance="minimal" iconBefore="edit" background="tint2">노트 추가하기</Button> */}
-          {/* </Pane> */}
         </Pane>
         <Pane flex={1} display="flex" flexDirection="column">
           <Pane padding={minorScale(4)} background="yellowTint">
@@ -118,9 +105,30 @@ export default class NotePage extends Component {
           <Pane padding={minorScale(4)} flex={1}>
             <Pane width={730} marginX="auto" marginTop={60}>
               <Pane marginBottom={45}>
-                <Heading size={900}>Getting Started(title)</Heading>
+                <Heading size={900}>
+                  {isLoading ? (
+                    <Text>title..</Text>
+                  ) : (
+                    <Editor
+                      value={noteTitle}
+                      onChange={({ value }) =>
+                        this._handleEditorChange({ value })
+                      }
+                    />
+                  )}
+                </Heading>
               </Pane>
-              <Pane>this is where a editor goes in</Pane>
+              <Pane>
+                {/* <Editor
+                  value={content}
+                  onChange={({ value }) =>
+                    this._handleEditorChange({
+                      value,
+                      type: "content"
+                    })
+                  }
+                /> */}
+              </Pane>
             </Pane>
           </Pane>
         </Pane>
